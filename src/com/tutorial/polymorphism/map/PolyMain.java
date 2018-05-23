c5261379d8327fbd7b24ac02f413b25ef36ef171
newlyfei <newlyfei@163.com>
2018-05-23 11:53:14 +0800

    Java多态

---
+++ src/com/tutorial/polymorphism/map/PolyMain.java
@@ -0,0 +1,27 @@
+package com.tutorial.polymorphism.map;
+
+import com.tutorial.polymorphism.map.PolyTool;
+
+public class PolyMain {
+
+    public PolyMain(){
+        PolyTool.init();
+    }
+
+    public PolyBean resolver(String json,String service){
+        PolyService polyService=PolyTool.getService(service);
+        return polyService.parseJson(json);
+
+    }
+
+    public static void main(String[] args) {
+        PolyMain polyMain=new PolyMain();
+
+        PolyBean polyBean=polyMain.resolver("{}","polyAService");
+        System.out.println(String.format("name=%s",polyBean.getName()));
+
+        polyBean=polyMain.resolver("{}","polyBService");
+        System.out.println(String.format("name=%s",polyBean.getName()));
+
+    }
+}
